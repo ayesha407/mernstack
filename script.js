5f89d9cdea02a252d1b0fc6e0137635f944b7622
ayesha407 <ayeshagudimetla@gmail.com>
2026-02-26 14:37:59 +0530

    commit

---
+++ script.js
@@ -0,0 +1,27 @@
+/*let a=10
+{
+    let a=20
+    console.log(a)
+}
+console.log(a)*/
+/*let obj1={
+    name: 'abc',
+    age:22
+}
+let obj2=obj1
+obj2.name='xyz';
+console.log(obj1.name)
+if(0)
+{
+    console.log('true')
+}
+if(1)
+{
+    console.log("false")
+}*/
+console.log(5+'5')
+console.log(5-'5');
+console.log(5=='5')
+
+let arr=[1,'new',2,'3']
+console.log(arr);
