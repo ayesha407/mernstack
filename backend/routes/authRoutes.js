d2788716b9766da8470f9d6ad8339773c05cbad5
ayesha407 <ayeshagudimetla@gmail.com>
2026-03-09 09:20:51 +0530

    msg

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,5 +3,7 @@ const router=express.Router()
 const {register}=require('../controller/authController')
 
  router.post('/register',register)
+ 
+ router.post('/login',login)
 
  module.exports=router
